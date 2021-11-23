665f8f3617452c0f9209f2305367a33a26c0b236
marquake <marcosms@gmail.com>
2021-11-23 17:51:59 +0100

    modificado proyecto para ejecutar test.

---
+++ app/src/test/java/p_jenk_mvn_java/app/PpalTest.java
@@ -0,0 +1,16 @@
+package p_jenk_mvn_java.app;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class PpalTest {
+
+	@Test
+	void testMain() {
+		//fail("Not yet implemented");
+		
+		Ppal ppal = new Ppal();
+	Assert.assertTrue(ppal != null);
+	}
+
+}
